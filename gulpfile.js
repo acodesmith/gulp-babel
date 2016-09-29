44298c4d5d0c2d1b40c035eecedd432cb4233648
Adam Smith <adam@wideopentech.com>
2016-09-29 18:10:04 -0400

    adding eslinter

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
   gulp.src('src/App.js')
+    .pipe(eslint())
     .pipe(babel({
       presets: ['es2015']
     }))
